1c264ee0b8cc647258ef1594e99d2c06ecd4d93a
Sasha <sashapysmenny@gmail.com>
2020-12-02 14:59:06 +0200

    refactored code && removed unwanted files

--- app/lib/logic.rb
+++ app/lib/logic.rb
@@ -1,24 +1,17 @@
 module Logic
   def self.change_params(rack, name)
-
     Rack::Response.new do |response|
       response.set_cookie(name, rack.cookies["#{name}"].to_i + 10) if rack.cookies["#{name}"].to_i < 100
-
       need = ($NEEDS - [name]).sample
-
       response.set_cookie(need, rack.cookies["#{need}"].to_i - 10)
-
       response.redirect('/start')
     end
   end
 
   def self.study(rack, name)
-
     Rack::Response.new do |responses|
       responses.set_cookie(name, rack.cookies["#{name}"].to_i + rand(-5..20))
-
       responses.redirect('/study')
     end
   end
-
 end
--- config.ru
+++ config.ru
@@ -2,7 +2,4 @@ require './app/bunny'
 
 use Rack::Reloader, 0
 use Rack::Static, urls: ["/public"]
-#use Rack::Auth::Basic do |username, password|
-#  username == "log" and password == "pass"
-#end
 run Bunny
